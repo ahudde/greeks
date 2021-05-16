ae9eb2f176ea71b2df14f97d6803f9b7287d03fd
anselmhudde <60978072+anselmhudde@users.noreply.github.com>
2021-05-16 19:37:25 +0200

    greeks_0.1.0
    Now also with European Malliavin Greeks and Gamma for Malliavin Asian Greeks

--- R/Malliavin_Asian_Greeks.R
+++ R/Malliavin_Asian_Greeks.R
@@ -1,5 +1,5 @@
-#' This function calculates the fair value of an Asian call- or put-option by
-#' with the Malliavin Monte Carlo Method in the Black Scholes model.
+#' This function calculates the fair value of an Asian option with the Malliavin
+#' Monte Carlo Method in the Black Scholes model.
 #' @export
 #'
 #' @importFrom "stats" "rnorm"
@@ -17,6 +17,9 @@
 #' @param steps - the number of integration steps.
 #' @param paths - the number of simulated paths.
 #' @param seed - the seed of the random number generator
+#' @param numeric_method - the numbers choosen for the numeric intergration,
+#' either a string in ("monte_carlo", "monte_carlo_antithetic", "equal_distance)
+#' or custum numbers in form of a matrix with nrows = paths
 #'
 #' @return Named vector containing the values of the Greeks specified in the
 #' parameter \code{greek}.
@@ -33,33 +36,48 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
                                    volatility = 0.3,
                                    dividend_yield = 0,
                                    payoff = "call",
-                                   greek = c("fair_value", "delta", "rho",
-                                             "theta"),
+                                   greek = c("fair_value", "delta", "rho", "vega",
+                                             "theta", "gamma"),
                                    model = "Black Scholes",
-                                   steps = time_to_maturity*252,
+                                   steps = round(time_to_maturity*252),
                                    paths = 10000,
-                                   seed = 1) {
+                                   seed = 1,
+                                   numeric_method = "monte_carlo") {
 
   dt <- 1/steps
-
-  ## the seed is just set locally
-  old <- .Random.seed
-  on.exit( { .Random.seed <<- old } )
-
-  if(!is.na(seed)) {
-    set.seed(seed)
-  }
 
   result <- vector(mode = "numeric", length = length(greek))
 
   names(result) <- greek
 
+  ## the seed is set locally
+
+  if(!is.na(seed)) {
+    old.seed <- .Random.seed
+    set.seed(seed)
+  }
+
   ## the increments of the Brownian motion ###
 
-  W <- rnorm(n = paths*steps, sd = sqrt(dt)) %>%
-    matrix(nrow=paths) %>%
-    apply(MARGIN=1, FUN=cumsum) %>%
-    t()
+  if(numeric_method == "monte_carlo") {
+    W <- rnorm(n = paths*steps, sd = sqrt(dt)) %>%
+      matrix(nrow = paths) %>%
+      apply(MARGIN = 1, FUN = cumsum) %>%
+      t()
+  } else if(numeric_method == "monte_carlo_antithetic") {
+    W <- rnorm(n = paths/2*steps, sd = sqrt(dt)) %>%
+      matrix(nrow = paths/2) %>%
+      apply(MARGIN = 1, FUN = cumsum) %>%
+      t()
+    W <- rbind(W, -W)
+  } else if(class(numeric_method) == "matrix" &&
+                   prod(dim(numeric_method) == c(steps, paths))) {
+    W <- numeric_method
+  }
+
+  if(!is.na(seed)) {
+    .Random.seed <<- old.seed
+  }
 
   W <- cbind(numeric(paths), W)
 
@@ -79,8 +97,6 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
       }
     }
 
-  ### the calculation of I_{(n)}, the integral \int_0^T t^n X_t dt ###
-
   if(model == "Black Scholes") {
 
     X <- matrix(nrow = paths, ncol = steps+1)
@@ -94,19 +110,27 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
     return()
   }
 
-  X_T <- X[, steps + 1]
+  X_T <- X[, steps+1]
 
-  I_0 <- dt * rowSums(X[, -1])
+  ### the calculation of I_{(n)}, the integral \int_0^T t^n X_t dt ###
 
-  I_1 <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt) * X[, -1])
+  I_0 <- (X[, 1]/2 + rowSums(X[, 2:steps]) + X[, steps+1]/2) * dt
 
-  I_2 <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt)^2 * X[, -1])
+  XW <- (X[, 1]*W[, 1]/2 + rowSums(X[, 2:steps]*W[, 2:steps]) +
+           X[, steps+1]*W[, steps + 1]/2) * dt
 
-  I_3 <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt)^3 * X[, -1])
+  tXW <- X[, steps+1]*W[, steps+1]/2 * (steps*dt) * dt
 
-  XW <- dt * rowSums(X[, -1] * W[, -1])
+  I_1 <- X[, steps+1]/2 * (steps*dt) * dt
+  I_2 <- X[, steps+1]/2 * (steps*dt)^2 * dt
+  I_3 <- X[, steps+1]/2 * (steps*dt)^3 * dt
 
-  tXW <- dt * rowSums(seq(from = dt, to = time_to_maturity, by = dt) * X[, -1] * W[, -1])
+  for(i in 1:(steps-1)) {
+    I_1 <- I_1 + X[, i+1] * (i*dt) * dt
+    I_2 <- I_2 + X[, i+1] * (i*dt)^2 * dt
+    I_3 <- I_3 + X[, i+1] * (i*dt)^3 * dt
+    tXW <- tXW + X[, i+1]*W[, i+1] * (i*dt) * dt
+  }
 
   E <- function(weight) {
     return(exp(-r*time_to_maturity) * mean(payoff(I_0/time_to_maturity) * weight))
@@ -119,8 +143,8 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
 
   if("delta" %in% greek) {
     result["delta"] <-
-      1/(volatility * initial_price) *
-      (-volatility + I_0/I_1*W_T + volatility*I_0*I_2/(I_1^2)) %>%
+      (1/(volatility * initial_price) *
+      (-volatility + I_0/I_1*W_T + volatility*I_0*I_2/(I_1^2))) %>%
       E()
   }
 
@@ -139,6 +163,27 @@ Malliavin_Asian_Greeks <- function(initial_price = 100,
       E()
   }
 
+  if("vega" %in% greek) {
+    result["vega"] <-
+    ((1 / volatility) *
+      ( -(1 + volatility * W_T) +
+          (W_T * XW - volatility * tXW) / I_1 +
+          (volatility * XW * I_2) / I_1^2)) %>%
+      E()
+  }
+
+  if("gamma" %in% greek) {
+    result["gamma"] <-
+      ((1/(volatility^2 * initial_price^2) *
+      (((1+x) * volatility^2) -
+      (((1+x)*W_T + 1) * volatility * I_0/I_1) +
+      ((W_T^2*I_0 - (2*volatility + (1+x)*volatility^2)*I_2) * (I_0/I_1^2)) +
+      ((W_T*I_2 + volatility*(2*I_2 - I_3)) * (I_0^2/I_1^3)) +
+      ((3*volatility * I_0^2*I_2^2) / I_1^4)))) %>%
+      E()
+  }
+
   return(result)
 
 }
+
