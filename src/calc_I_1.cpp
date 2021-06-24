df4db6baa62afcdc4190c230a70136e6d7d16c1a
anselmhudde <60978072+anselmhudde@users.noreply.github.com>
2021-06-24 23:52:42 +0200

    Add files via upload

---
+++ src/calc_I_1.cpp
@@ -0,0 +1,15 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+//' @export
+// [[Rcpp::export]]
+NumericVector calc_I_1(NumericMatrix X, int steps, float dt) {
+  int i;
+  NumericVector I;
+  I = X(_, steps)/2 * steps * dt * dt;
+  for(i = 1; i < steps; i++) {
+    I = I + X(_, i) * (i * dt) * dt;
+    }
+  return I;
+}
